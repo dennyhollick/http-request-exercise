3067af8e9ed49adb4e6d96a92e4e62f6ca215406
Denny <dennyhollick@gmail.com>
2017-07-03 19:15:15 +0000

    initial commit with working chunker file

---
+++ get-and-print.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+function getAndPrintHTMLChunks () {
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+}
+
